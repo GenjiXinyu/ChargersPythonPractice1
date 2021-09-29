572bfde7a845e629093b8d938b98370f0b670e87
GenjiXinyu <52012501+GenjiXinyu@users.noreply.github.com>
2021-09-28 22:49:20 -0700

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,65 @@
+print("Part 1: Your Name")
+# Asks the user for their name
+# Greet them using their name "Hello Kenji" for example
+# Print "We have the same name!" if user enters your name
+# ********
+
+# ********
+quit("Delete this line once you finish the given task")
+print("\n")
+
+print("Part 2: Graduating Year")
+year = 2021
+# Asks the user for their grade and make sure the value is an integer
+# Use a loop to find which grade you will be in the upcoming years and print "In 2021, I will be 11th grade" for example
+# End loop once you reach 12th grade
+# ********
+
+# ********
+quit("Delete this line once you finish the given task")
+print("\n")
+
+print("Part 3: Lucky Number")
+number = 3.1415
+luckyNumber = int(input("What is your lucky number? "))
+# Use operators on the number variable to make it equal to user input of their lucky number
+# The if conditional will then check if the number variable is an even or odd number
+# ********
+
+# ********
+if number == luckyNumber:
+  # Check if the number is even or odd and print the result
+  # ********
+
+  # ********
+  print()
+else:
+  print("Try again")
+  quit()
+quit("Delete this line once you finish the given task")
+print("\n")
+
+print("Part 4: Secret Code")
+infiniteLoop = True
+# Change these boolean values so that infiniteLoop can be switched to False
+# ********
+number_1 = None
+number_2 = None
+number_3 = None
+number_4 = None
+number_5 = None
+# ********
+# Use ctr+c in order to exit out of your program and stop the infinite loop
+while infiniteLoop:
+  if number_1:
+    if not number_2:
+      if not number_3:
+        if number_4:
+          if not number_5:
+            infiniteLoop = False
+  # Use ctr+c in order to exit out of your program and stop the infinite loop
+  print("Try to break out")
+print("You are finally out!!!")
+print("\n")
+
+print("Congratulations on completing all 4 practices!!!")
